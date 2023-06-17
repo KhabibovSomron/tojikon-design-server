1b367c196f47bc648873265093c7a6d1cbcef0de
KhabibovSomron <habibov200@gmail.com>
2023-06-17 03:56:02 +0300

    Модели добавлены в админку

--- tojikon_design/project/admin.py
+++ tojikon_design/project/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Project, Image, Category, Video 
 
 # Register your models here.
+
+admin.site.site_header = "Tojikon Design Administration"
+admin.site.unregister(Group)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'created_date')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project')
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project', 'link')
+
