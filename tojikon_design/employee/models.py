6acd3b01a3be7d17ca1be7ca00cf8bed5bb8ed05
KhabibovSomron <habibov200@gmail.com>
2023-06-17 03:34:51 +0300

    Созданы модели для описаний проектов, категорий, изображений проектов, видеоматериалы проектов и сотрудников

--- tojikon_design/employee/models.py
+++ tojikon_design/employee/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField("ФИО", max_length=255)
+    specialization = models.CharField("Специализация", max_length=100)
+    email = models.CharField("Электронная почта", max_length=100)
+    about = models.CharField("О сотруднике", max_length=255)
+    image = models.ImageField("Изображение", upload_to="images/employees/", default="none")
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.specialization}"
+    
+
+    class Meta:
+        verbose_name = "Сотрудники"
+        verbose_name_plural = "Сотрудники"
--- tojikon_design/project/models.py
+++ tojikon_design/project/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField("Название категорий", max_length=255)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Категорий"
+        verbose_name_plural = "Категорий"
+
+
+class Project(models.Model):
+    title = models.CharField("Название проекта", max_length=255)
+    description = models.TextField("Описание проекта")
+    created_date = models.DateField("Дата добавление проекта в бд", auto_now_add=True)
+    finish_date = models.DateField("Дата окончания проекта")
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Проекты"
+        verbose_name_plural = "Проекты"
+
+
+class Image(models.Model):
+    project = models.ForeignKey(Project, verbose_name="Проект", on_delete=models.CASCADE)
+    image = models.ImageField("Изображение проекта", upload_to="images/projects/", default="none")
+
+    def __str__(self):
+        return self.project
+    
+    class Meta:
+        verbose_name = "Изображений проекта"
+        verbose_name_plural = "Изображений проекта"
+
+
+class Video(models.Model):
+    project = models.ForeignKey(Project, verbose_name="Проект", on_delete=models.CASCADE)
+    link = models.CharField("Ссылка на видео из Youtube", max_length=500, default="")
+    
+    def __str__(self):
+        return self.project
+    
+    class Meta:
+        verbose_name = "Видео"
+        verbose_name_plural = "Видео"
